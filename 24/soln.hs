a232ba80ea0893def5e97ab27c616e436861e79e
Bart Massey <bart@cs.pdx.edu>
2016-01-03 16:40:50 -0800

    made a try at a best-fit solution for 24; should be fast once product pruning is added

--- 24/soln.hs
+++ 24/soln.hs
@@ -11,55 +11,89 @@ parseWeights :: String -> [Int]
 parseWeights stuff =
     map read $ lines stuff
 
--- | Generate all the asymmetric splits of the first few
--- elements. This removes some symmetries from the overall
--- problem.
-asymmetricSplits :: Int -> Int -> [Int] -> [[[Int]]]
-asymmetricSplits n _ [] = [replicate n []]
-asymmetricSplits n maxWeight (w : ws) = nub $ do
-  partial <- asymmetricSplits n maxWeight ws
-  (first, bin : rest) <- splits partial
-  let newWeight = sum bin + w
-  guard $ newWeight <= maxWeight
-  guard $ all (newWeight <=) (map sum rest)
-  return (first ++ [w : bin] ++ rest)
+-- | A bin, with its current total weight tracked.
+data Bin = Bin {
+      binWeight :: Int,
+      binPackages :: [Int] }
 
--- | Generate all the valid splits.
-allSplits :: Int -> Int -> [Int] -> [[[Int]]]
-allSplits n maxWeight ws | length ws <= n =
-    asymmetricSplits n maxWeight ws
-allSplits _ _ [] = error "unobtainium"
-allSplits n maxWeight (w : ws) = do
-  partial <- partials
-  (first, bin : rest) <- splits partial
-  guard $ sum bin + w <= maxWeight
-  return (first ++ [w : bin] ++ rest)
-  where
-    partials = allSplits n maxWeight ws
+-- | Insert a package into a bin.
+insertPackage :: Int -> Bin -> Bin
+insertPackage p bin =
+    Bin { binWeight = p + binWeight bin,
+          binPackages = p : binPackages bin }
+
+-- | The empty bin.
+emptyBin :: Bin
+emptyBin = Bin 0 []
+
+-- | Pack up packages into bins. Return a list of packings
+-- consisting of the target weight packing and the remaining
+-- unpacked packages.
+packBin :: Int -> [Int] -> [([Int], [Int])]
+packBin targetWeight toPack =
+    catMaybes $ map getSolution $
+      foldr packPackage [(emptyBin, emptyBin)] toPack
+    where
+      totalWeight = sum toPack
+      getSolution (packed, unpacked)
+          | binWeight packed == targetWeight =
+              Just (binPackages packed, binPackages unpacked)
+          | otherwise = Nothing
+      packPackage p partials =
+          concatMap packPartial partials
+          where
+            packPartial (packed, unpacked)
+                | p + packedWeight + remainingWeight < targetWeight = []
+                | p + packedWeight > targetWeight = [dontPack]
+                | otherwise = [doPack, dontPack]
+                where
+                  packedWeight = binWeight packed
+                  remainingWeight = totalWeight - binWeight unpacked
+                  doPack = (insertPackage p packed, unpacked)
+                  dontPack = (packed, insertPackage p unpacked)
+
+
+-- Partition a collection of packages into `n` bins, each with
+-- the given target weight, in every way possible.
+packBins :: Int -> Int -> [Int] -> [[[Int]]]
+packBins n targetWeight packages =
+    map complete $ (iterate packNext [([], packages)] !! (n - 1))
+    where
+      complete :: ([[Int]], [Int]) -> [[Int]]
+      complete (partial, remaining) =
+          remaining : partial
+      packNext :: [([[Int]], [Int])] -> [([[Int]], [Int])]
+      packNext partials =
+          concatMap packPartial partials
+          where
+            packPartial (packed, remaining) =
+                map recordPacking $ packBin targetWeight remaining
+                where
+                  recordPacking (packing, remaining') =
+                      (packing : packed, remaining')
+                
+-- | Find a packing with a least-entangled bin. Return the
+-- entanglement of that bin.
+leastEntanglement :: [[[Int]]] -> Integer
+leastEntanglement packings =
+    let scorePacking = minimum . map (product . map fromIntegral) in
+    minimum $ map scorePacking packings
 
 -- | Strategy: Brute force.
-soln :: Int -> String -> IO ()
+soln :: Int -> String -> [[Int]]
 soln n stuff = do
-  let weights = sort $ parseWeights stuff
-  let totalWeight = sum weights
+  let packages = sort $ parseWeights stuff
+  let totalWeight = sum packages
   let sectionWeight
           | totalWeight `mod` n == 0 = totalWeight `div` n
           | otherwise = error "bad parity"
-  let packings = allSplits n sectionWeight weights
-  print $ best packings
-  where
-    best :: [[[Int]]] -> Integer
-    best packings =
-        let scorePacking = minimum . map (product . map fromIntegral)
-            scores = map scorePacking packings
-        in
-        minimum scores
+  head $ packBins n sectionWeight packages
 
 solna :: String -> IO ()
-solna stuff = soln 3 stuff
+solna stuff = print $ soln 3 stuff
 
 solnb :: String -> IO ()
-solnb stuff = soln 4 stuff
+solnb stuff = print $ soln 4 stuff
 
 main :: IO ()
 main = makeMain solna solnb
